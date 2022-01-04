0b4584b888939e5540a23737bdc65627ad4c99ed
Muhamad Rizaludin <muhamadrizaludin24@gmail.com>
2022-01-04 11:20:13 +0700

    first commit
    upload file project sistem informasi rumah sakit menggunakan php native bootsrap templating and database mysql

---
+++ auth/login.php
@@ -0,0 +1,41 @@
+<?php
+require_once "../_config/config.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Loging - Rumah Sakit</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="../_assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" href="<?base_url()?>/_assets/img/day.jpg">
+</head>
+
+<body>
+    <div id="wrapper">
+        <div class="container">
+            <div align="center" style="margin-top: 210px;">
+                <form action="" method="POST" class="navbar-form" >
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input type="text" name="user" class="form-control" placeholder="Username" required autofocus>
+                    </div>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <input type="password" name="pass" class="form-control" placeholder="Password" required>
+                    </div>
+                    <div class="input-group">
+                        <input type="submit" name="login" class="btn btn-primary" value="login">
+                    </div>
+                </form>
+            </div>   
+        </div>
+    </div>
+</body>
+
+</html>
