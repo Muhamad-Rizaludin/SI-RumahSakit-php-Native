46761c00b843901d50577a18846e37ca8fb02b08
Muhamad Rizaludin <muhamadrizaludin24@gmail.com>
2022-01-04 21:16:38 +0700

    update-index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 require_once "_config/config.php";
-if(isset($_SESSION['user'])){
-   echo "hello world. <a href=\"auth/logout.php\">Logout</a>;
+if(isset($_SESSION['user'])) {
+   echo "hello world. <a href=\"auth/logout.php\">Logout</a>";
 } else {
   echo "<script>window.location='".base_url('auth/login.php')."';</script>";
- }
+}
 ?>
 
